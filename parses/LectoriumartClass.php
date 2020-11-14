d5b26ef5ae9cb4d841866e8d46e71588f10e08bf
devweb99 <kibelov.it@webzaim.ru>
2020-11-14 05:19:55 +0000

    save

---
+++ parses/LectoriumartClass.php
@@ -0,0 +1,19 @@
+<?php
+
+class LectoriumartClass {
+    private $id;
+    private $uri;
+    
+    public function __construct () {}
+
+    public function getPreview ()
+    {
+
+    }
+
+    public function getDetail ()
+    {
+
+    }    
+}
+
